8adfcb8c52441ae380e88e7f44f48a69adc89113
Yoshi2 <debugyoshi@gmail.com>
2019-09-14 13:41:44 +0200

    xer register and changes to conditional register

--- machine.py
+++ machine.py
@@ -18,8 +18,13 @@ class ConditionalRegister(object):
     
     def __setitem__(self, index, value):
         assert value in (LT, GT, EQ, SO, 0)
-        self.cr[index] = value 
-    
+        if value == SO:
+            # SO bit is independent of comparison bits
+            self.cr[index] = value | SO 
+        else:
+            # SO bit can only be cleared by MTSPR/MCRXR
+            self.cr[index] = value | (self.cr[index] & SO)
+        
     def from_value(self, val):
         for i in range(8):
             bitfield = (val >> (7-i)*4) & 0b1111 
@@ -29,6 +34,9 @@ class ConditionalRegister(object):
         val = 0
         for i in range(8):
             val = val | (self.cr[i] << (7-i)*4)
+    
+    def clear(self, index):
+        self.cr[index] = 0
     
     def is_equal(self, index):
         return self.cr[index] == EQ 
@@ -42,20 +50,50 @@ class ConditionalRegister(object):
     def __str__(self):
         out = ""
         for i in range(8):
-            if self.cr[i] == LT:
+            compare_result = self.cr[i] & ~SO 
+            so_bit = self.cr[i] & SO 
+            
+            if compare_result == LT:
                 out += "CR{0}: LT, ".format(i)
-            elif self.cr[i] == GT:
+            elif compare_result == GT:
                 out += "CR{0}: GT, ".format(i)
-            elif self.cr[i] == EQ:
+            elif compare_result == EQ:
                 out += "CR{0}: EQ, ".format(i)
-            elif self.cr[i] == SO:
-                out += "CR{0}: SO, ".format(i)
             else:
-                out += "CR{0}: 0, ".format(i)
+                raise RuntimeError("Invalid compare result: {0}".format(compare_result))
+                
+            if so_bit:
+                out += "SO, "
         
         return out 
+
+
+class XerRegister(object):
+    def __init__(self):
+        self.SO = 0 # summary overflow when instruction sets OV and remains until cleared 
+        self.OV = 0 # overflow, indicate that overflow occured during execution of an instruction 
+        self.CA = 0 # carry, set if there is a carry out of the msb 
+        self.reserved = 0
+        self.bytes_transfered = 0
         
+    def from_value(self, val):
+        self.bytes_transfered = val & 0b1111111 # 7 bits 
+        self.reserved = (val >> 7) & 0b1111111111111111111111 # 22 bits 
+        self.CA = (val >> 29) & 1 
+        self.OV = (val >> 30) & 1 
+        self.SO = (val >> 31) & 1 
+    
+    def to_value(self):
+        val = 0
+        val = val | (self.bytes_transfered & 0b1111111)
+        val = val | (self.reserved & 0b1111111111111111111111) << 7 
+        val = val | (self.CA & 1) << 29 
+        val = val | (self.OV & 1) << 30
+        val = val | (self.SO & 1) << 31 
         
+        return val 
+        
+    
 class Machine(object):
     def __init__(self):
         pass
